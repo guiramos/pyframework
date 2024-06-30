bc5e5d4d5f007d4db36b3e3a4844c6d8d03946af
Guilherme Ramos <guiramos@gmail.com>
2024-06-30 18:29:53 -0300

    Updating gitignore

--- pyframework/logging.py
+++ pyframework/logging.py
@@ -7,6 +7,6 @@ log_formatter = logging.Formatter(log_format)
 # logging.basicConfig(level=logging.INFO, format=log_format)
 log_max_size = 10 * 1024 * 1024  # 10 MB
 log_backup_count = 5  # keep 5 backup logs
-app_log_file = 'files/app.log'
+app_log_file = 'logs/app.log'
 rolling_file_handler = RotatingFileHandler(app_log_file, maxBytes=log_max_size, backupCount=log_backup_count)
 rolling_file_handler.setFormatter(log_formatter)
