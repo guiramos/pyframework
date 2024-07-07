d66d8464ca7d3991a1f022bfb2f9c9c2e38325c9
Guilherme Ramos <guiramos@gmail.com>
2024-07-07 19:25:32 -0300

    Adding default uvicorn logging configuration

--- pyframework/logging.py
+++ pyframework/logging.py
@@ -10,3 +10,27 @@ log_backup_count = 5  # keep 5 backup logs
 app_log_file = 'logs/app.log'
 rolling_file_handler = RotatingFileHandler(app_log_file, maxBytes=log_max_size, backupCount=log_backup_count)
 rolling_file_handler.setFormatter(log_formatter)
+
+
+uvcorn_logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            # Using fixed-width formatting for each field
+            "format": "%(asctime)s - %(levelname)6s - %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",  # Default is sys.stderr
+        },
+    },
+    "loggers": {
+        "uvicorn": {"handlers": ["console"], "level": "INFO"},
+        "uvicorn.error": {"level": "INFO"},
+        "uvicorn.access": {"handlers": ["console"], "level": "INFO", "propagate": False},
+    },
+}
